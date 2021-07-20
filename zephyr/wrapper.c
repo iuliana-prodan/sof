f8197a299baa32c436fa62a692b54b35b9b0a1e1
Iuliana Prodan <iuliana.prodan@nxp.com>
2021-07-20 22:24:43 +0300

    notifier_register for SOF_SCHEDULE_LL_DMA
    Signed-off-by: Iuliana Prodan <iuliana.prodan@nxp.com>

--- zephyr/wrapper.c
+++ zephyr/wrapper.c
@@ -612,9 +612,15 @@ int task_main_start(struct sof *sof)
 	pipeline_posn_init(sof);
 
 	/* Temporary fix for issue #4356 */
+#if defined(CONFIG_IMX)
+	(void)notifier_register(NULL, scheduler_get_data(SOF_SCHEDULE_LL_DMA),
+				NOTIFIER_ID_LL_POST_RUN,
+				ipc_send_queued_callback, 0);
+#else
 	(void)notifier_register(NULL, scheduler_get_data(SOF_SCHEDULE_LL_TIMER),
 				NOTIFIER_ID_LL_POST_RUN,
 				ipc_send_queued_callback, 0);
+#endif
 
 	/* let host know DSP boot is complete */
 	ret = platform_boot_complete(0);
